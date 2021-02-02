12d69c427985b32e38bd00b3b11192a52b246eec
NathanStiers <37657141+NathanStiers@users.noreply.github.com>
2021-02-02 11:27:18 +0100

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,16 +6,16 @@ var express = require('express')
 var router = express.Router()
 //const db = require('../modules/db')
 
-var tasksList = ["Faire les courses","Nourir le chat"];
+var tasksList = {tasks : ["Faire les courses","Nourir le chat"]};
 
 router.get('/add/:task', function (req, res) {
-    tasksList.push(req.params.task);
+    tasksList.tasks.push(req.params.task);
     res.setHeader('Content-Type', 'application/json');
     res.json(tasksList);
 })
 
 router.get('/delete/:id', function (req, res) {
-    tasksList.splice(req.params.id,1);
+    tasksList.tasks.splice(req.params.id,1);
     res.setHeader('Content-Type', 'application/json');
     res.json(tasksList);
 })
