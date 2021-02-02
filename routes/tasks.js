2fd195e805546c0a0c5010abc5202cfa9a24f992
NathanStiers <37657141+NathanStiers@users.noreply.github.com>
2021-02-02 01:29:49 +0100

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -10,17 +10,20 @@ var tasksList = ["Faire les courses","Nourir le chat"];
 
 router.get('/add/:task', function (req, res) {
     tasksList.push(req.params.task);
-    res.send(tasksList);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ tasksList }));
 })
 
 router.get('/delete/:id', function (req, res) {
     tasksList.splice(req.params.id,1);
-    res.send(tasksList);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ tasksList }));
 })
 
 router.get('/fetch', function (req, res) {
     console.info("fetching")
-    res.send(tasksList);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ tasksList }));
 })
 
 /**
